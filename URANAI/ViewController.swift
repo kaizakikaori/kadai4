3dadd64579f07cbc5c235c60cc13996dde8894f4
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-16 02:32:12 +0900

    first commit

---
+++ URANAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URANAI
+//
+//  Created by VERTEX21 on 2019/08/16.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
