8190590311ba5b56d0f16b61304f6ea238963beb
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-16 03:38:09 +0900

    sliderの数字の設定を修正、アプリ完成

--- URANAI/ViewController.swift
+++ URANAI/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // ★紐付け一覧
+    // 紐付け
     // 誕生日を入力するPickerViewを紐付け
     @IBOutlet weak var birthDayPicker: UIDatePicker!
     var sliderNum: Int = 50
@@ -18,17 +18,13 @@ class ViewController: UIViewController {
     // 血液型を選択するSegmentControllを紐付け
     @IBOutlet weak var bloodType: UISegmentedControl!
     
-    
     // Sliderから好きな数字を選ぶとSliderの下のラベルに数字が反映される
-    
-    
     @IBAction func likeNum(_ sender: UISlider) {
         // 下のラベル(showNum)に文字を反映させる処理を書く
         // sender.valueの初期値はDouble型なのでInt型へキャストする
         sliderNum = Int(sender.value)
         // textに代入するためにSrting型にキャストする
         showNum.text = String(sliderNum)
-        
     }
     
     // Sliderで選んだ数字を表示させるラベルを紐付け
@@ -36,10 +32,7 @@ class ViewController: UIViewController {
     // 占い結果を表示するTextViewの紐付け
     @IBOutlet weak var result: UITextView!
     
-    
-    // ★変数、定数一覧
-    
-    
+    // 変数、定数など
     //  各選択肢の占い結果を配列で定義
     // 誕生日の選択結果
     let pickerResult : [String] = [
@@ -65,7 +58,7 @@ class ViewController: UIViewController {
         "ふと口をついて出た曲をダウンロードするか、ラジオ番組にリクエストメールを出しましょう。",
     ]
     
-    // ★関数
+    // 関数など
     // 画面が読み込まれたときの処理
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,7 +66,7 @@ class ViewController: UIViewController {
     
     // birthDayPickerViewの選択結果を、占い結果を決めるkeyに変換し、戻り値で取得する(returnするので、戻り値の型指定も忘れないこと!!)
     
-    func setPicker() -> Int{
+    func setPicker() -> Int {
         // まず、birthDayPickerViewの全てのデータを格納する(PickerViewの紐付け時に型をUIDatePicker!にすること)
         let date = birthDayPicker.date
         
@@ -84,7 +77,6 @@ class ViewController: UIViewController {
         
         // 占い結果(に使うため、4で割った余りを定数に格納(4パターンある)
         let key: Int = (year + month + day) % 4
-        
         return key
     }
     
@@ -106,9 +98,7 @@ class ViewController: UIViewController {
     
     //「占うボタン」をおしたとき、占い結果を最下部のtextView(result)に表示する
     @IBAction func finalResult(_ sender: Any) {
-        
         result.text = pickerResult[setPicker()] + bloodResult[setBlood()] + likeNumResult[setLikeNum()]
-        
     }
 }
 
